31dd39a4c70351129e8d9509eefe254856fb8c00
Keith Grable <kw28grab@siena.edu>
2019-05-18 02:15:29 -0400

    Swapping axes

--- src/stats/TheCircle.java
+++ src/stats/TheCircle.java
@@ -11,7 +11,7 @@ package stats;
  */
 public class TheCircle {
     
-    private static final int SIZE = 30;
+    private static final int SIZE = 100;
     private static final double[][] PROBS = new double[SIZE + 1][SIZE + 1];
     static {
         for (int n = 0; n <= SIZE; n++) {
@@ -26,18 +26,23 @@ public class TheCircle {
     } 
     
     public static void main(String[] args) {
-        // Print choices header
-        for (int c = 1; c <= SIZE; c++) {
-            System.out.printf("\t\tCh %d", c);
+        // Print options header
+        for (int n = 1; n <= SIZE; n++) {
+            System.out.printf("\tOpts %d", n);
         }
         System.out.printf("\n");
         
         // Print table
-        for (int n = 1; n <= SIZE; n++) {
-            System.out.printf("Ops %d\t", n);
+        for (int c = 1; c <= SIZE; c++) {
+            System.out.printf("Ch %d", c);
             
-            for (int c = 1; c <= n; c++) {
-                System.out.printf("\t%.12f", getProb(n,c));
+            for (int n = 1; n <= SIZE; n++) {
+                if(n < c) {
+                    System.out.printf("\t");
+                }
+                else {
+                    System.out.printf("\t%.12f", getProb(n,c));
+                }
             }
             System.out.printf("\n");
         }
